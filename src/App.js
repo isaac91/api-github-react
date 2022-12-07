aab9e776ecf461a842ec6d8c1edc623837ff390f
Alberto Isaac Ramos Saldaña <isaac.rasa@gmail.com>
2022-12-06 22:33:51 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
 import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component";
 import { Octokit } from "octokit";
-
 
 function App() {
   const [searchField, setSearchField] = useState("");
@@ -12,7 +11,7 @@ function App() {
 
   useEffect(() => {
     const octokit = new Octokit({
-      auth: "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",
+      auth: process.env.REACT_APP_GITHUB_TOKEN,
     });
 
     async function fetchData() {
@@ -40,20 +39,14 @@ function App() {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    if (searchField.length < 1) {
-      setFilteredIssues([]);
-      return;
-    }
+  const onInputSearchChange = (event) => {
+    const result = event.target.value;
+    setSearchField(result);
+
     const newfilteredIssues = issues.filter((issue) => {
       return issue.title.includes(searchField.toLocaleLowerCase());
     });
     setFilteredIssues(newfilteredIssues);
-  }, [searchField, issues]);
-
-  const onInputSearchChange = (event) => {
-    const result = event.target.value;
-    setSearchField(result);
   };
 
   return (
@@ -62,10 +55,9 @@ function App() {
         onChangeInputHandler={onInputSearchChange}
         placeholder="search for an issue"
       />
-      {filteredIssues.length > 0 && (
+      {searchField.length > 0 && (
         <CardList filteredIssues={filteredIssues} />
       )}
-    
     </div>
   );
 }
